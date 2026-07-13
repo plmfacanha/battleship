1277c7622a9904a6270a95cdff9c07112f6e6296
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-07-13 14:55:43 -0700

    feat: finish second unit test for isSunk()

--- ship.test.js
+++ ship.test.js
@@ -8,3 +8,13 @@ test("hit() called twice increase the damage by 2", () => {
 
   expect(ship.damage).toBe(2);
 });
+
+test("check if ship object is sunk after method hit() reaches its length", () => {
+  const ship = new Ship(3);
+
+  ship.hit();
+  ship.hit();
+  ship.hit();
+
+  expect(ship.isSunk()).toBeTruthy();
+});
